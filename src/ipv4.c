421294c33c68e019476325a0c8c4c36f41501e5f
Damian Budelewski <budelewski.damian@gmail.com>
2019-05-07 16:31:12 +0200

    Define lacking fields, fixed old ones

--- src/ipv4.c
+++ src/ipv4.c
@@ -3,42 +3,71 @@
 
 void c_ip(unsigned char *sendbuf, struct ifreq if_ip,int tx_len){
    	char input_char[32];
-	int input_int;
+	int input_int = 0;
 	struct iphdr *iph = (struct iphdr *) (sendbuf + sizeof(struct ether_header));
+
 
    	/* IP Header */
 	printf("---------------- IPv4 -----------------\n");
-	printf("input 0 to choose default options\n");
-	input_int = 0;
+	printf("0 for default\n");
+
+
+	/* Type of service */
 	printf("Type of service: ");
 	scanf("%d",&input_int);
 	if (input_int!=0){
-		iph->tos = htons(input_int);
+		iph->tos = input_int;
+		input_int = 0;
 	}
-	else iph->tos = 16;
-	input_int = 0;
-	printf("Id: ");
+	else iph->tos = 0;
+
+
+	/* Flags */
+	printf("Don't fragment (0,1): ");
+	scanf("%d",&input_int);
+	if (input_int!=0){
+		iph->frag_off = 0b01000000;
+		input_int = 0;
+	}
+	else iph->frag_off = 0;
+
+
+
+
+	/* Identification */
+    printf("Identification: ");
 	scanf("%d",&input_int);
 	if (input_int!=0){
 		iph->id = htons(input_int);
+		input_int = 0;
 	}
-	else iph->id = htons(50000); //Function to convert between host and network byte order (htons)
-	input_int = 0;
+	else iph->id = htonl(54321); 
+
+
+	/* Time to live */
 	printf("Time to live: ");
 	scanf("%d",&input_int);
 	if (input_int!=0){
-		iph->ttl = htons(input_int);
+		iph->ttl = input_int;
+		input_int = 0;
 	}
 	else iph->ttl = 128;
-	input_int = 0;
+
+
+	/* Destination IP address */
     printf("Destination IP address: ");
     scanf("%s",&input_char);
-	iph->daddr = inet_addr(input_char);
+	if (input_char!="0"){
+		iph->daddr = inet_addr(input_char);
+	}
+	else iph->daddr = inet_addr("127.0.0.1");
 
 
+	/* Those parametrs can only be filled that way */
+	iph->tot_len = htons(sizeof(struct iphdr) + sizeof (struct tcphdr));
+	iph->version = 4; // IPv4
+	iph->check = 0; // Set to 0 before calculating checksum
 	iph->ihl = 5;
-	iph->tot_len = htons(tx_len - sizeof(struct ether_header));
-	iph->version = 4;
 	iph->protocol = 6; // TCP
 	iph->saddr = inet_addr(inet_ntoa(((struct sockaddr_in *)&if_ip.ifr_addr)->sin_addr));
 }

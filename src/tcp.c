37eb6de76e57e9667b46c96a80dee79c957153a6
Damian Budelewski <budelewski.damian@gmail.com>
2019-05-07 23:13:29 +0200

    Added lacking fields

--- src/tcp.c
+++ src/tcp.c
@@ -5,19 +5,59 @@ void c_tcp(unsigned char *sendbuf){
  	struct tcphdr *tcph = (struct tcphdr *) (sendbuf + sizeof(struct ether_header) + sizeof(struct iphdr));
 	int input;
 
-
+	printf("---------------- TCP ------------------\n");
     printf ("Source Port : ");
     scanf("%d", &input);
-	tcph->source = htons(input);
+	if (input!=0){
+    	tcph->source = htons(input);
+        input = 0;
+    }
+    else tcph->source = htons(80);
+
     printf ("Destination Port : ");
     scanf("%d", &input);
-	tcph->dest = htons(input);
+	if (input!=0){
+    	tcph->dest = htons(input);
+    }
+    else tcph->dest = htons(80);
+
+
     printf ("Sequence number: ");
     scanf("%d",&input);
-    tcph->seq = htons(input);
+    tcph->seq = input;
+
     printf ("Acknowledgment number: ");
     scanf("%d",&input);
-    tcph->ack_seq = htons(input);
+    tcph->ack_seq = input;
+
+    printf ("FIN: ");
+    scanf("%d",&input);
+   	tcph->fin=input;
+    
+    printf ("SYN: ");
+    scanf("%d",&input);
+	tcph->syn=input;
+
+    printf ("RST: ");
+    scanf("%d",&input);
+	tcph->rst=input;
+
+    printf ("PUSH: ");
+    scanf("%d",&input);
+	tcph->psh=input;
+
+    printf ("ACK: ");
+    scanf("%d",&input);
+	tcph->ack=input;
+
+    printf ("URG: ");
+    scanf("%d",&input);
+	tcph->urg=input;
+
+
+    tcph->doff = 5; // tcp header size
+    tcph->window = htons(5840); // Maximum allowed window size 
+    tcph->check = 0; // will be calculate later 
 }
 
 
